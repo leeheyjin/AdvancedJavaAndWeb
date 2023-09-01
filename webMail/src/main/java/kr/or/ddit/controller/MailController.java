f64ec15b34d356d8c36cc82561e39008da4cc5db
leeheyjin <amideveloper@naver.com>
2023-09-01 09:06:04 +0900

    230831

--- webMail/src/main/java/kr/or/ddit/controller/MailController.java
+++ webMail/src/main/java/kr/or/ddit/controller/MailController.java
@@ -2,11 +2,14 @@ package kr.or.ddit.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -17,7 +20,7 @@ import java.util.Map;
 @RequestMapping("/mail")
 @RequiredArgsConstructor
 public class MailController {
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
     @GetMapping("/sendMail")
     public String getSendMail() {
@@ -25,23 +28,22 @@ public class MailController {
     }
 
     @PostMapping("/sendMail")
-    public String postSendMail(@RequestParam Map<String, String> map) {
+    public String postSendMail(@RequestParam Map<String, String> map) throws MessagingException {
         log.info("MAP: " + map);
-
         String subject = map.get("subject");
-        String content = map.get("content");
-        String receiver = map.get("receiver");
-        String sender = map.get("sender");
+        String text = map.get("text");
+        String to = map.get("to");
+        String from = map.get("from");
 
-        MimeMessage mail = mailSender.createMimeMessage();
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mail, true, "UTF-8");
-            mimeMessageHelper.setFrom(sender);
-            mimeMessageHelper.
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "utf-8");
+        messageHelper.setSubject(subject);
+        messageHelper.setText(text);
+        messageHelper.setTo(to);
+        messageHelper.setFrom(from);
 
-    }
-
+        mailSender.send(message);
+        log.info("메일 전송 완료");
+        return "redirect:/mail/sendMail";
+     }
 }
